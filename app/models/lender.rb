ae0d0b1c0c1dfdcb1623794e5eabbb8b5c97f73c
SakthivelC <sakthislm@gmail.com>
2012-03-18 00:06:04 +0530

    Signup links added to home page

--- app/models/lender.rb
+++ app/models/lender.rb
@@ -5,7 +5,7 @@ class Lender < ActiveRecord::Base
     validates :phone,  presence: true, length: { maximum: 10 }, format: {with:valid_phone_regex}
     valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: { with: valid_email_regex }, uniqueness: { case_sensitive: false }
-    validates :village,  presence: true, length: { maximum: 25 }
+    validates :street,  presence: true, length: { maximum: 25 }
     validates :taluk,  presence: true, length: { maximum: 25 }
     validates :district,  presence: true, length: { maximum: 20 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Proj1Primal::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/farmersignup', to: 'farmers#new'
-
+  match '/lendersignup', to: 'lenders#new'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
