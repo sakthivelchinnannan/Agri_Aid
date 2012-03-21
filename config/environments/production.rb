186f838beee2b8b446bac38023a92ffe5ac94ad3
Sakthivel Chinnnannan <sakthislm@gmail.com>
2012-03-21 23:02:23 +0530

    Precompile Blueprint CSS in production(config/environments/production.rb)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,7 @@ Proj1Primal::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Precompile Blueprint CSS
+  config.assets.precompile += [ "blueprint/*.css" ]
 end
